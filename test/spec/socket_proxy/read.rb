ecd89ef78dafa5dc397d099ebea6d38d064044d0
ntl <nathanladd+github@gmail.com>
2016-01-23 17:57:07 -0600

    Improve test output

--- test/spec/socket_proxy/read.rb
+++ test/spec/socket_proxy/read.rb
@@ -1,80 +1,82 @@
 require_relative './socket_proxy_spec_init'
 
-context 'Reading From a Socket Proxy' do
-  data = Networking::Controls::Data.example
+context 'Socket Proxy' do
+  context 'Writing' do
+    data = Networking::Controls::Data.example
 
-  test 'Read until end of file is reached' do
-    io = Networking::Controls::IO::Reading.example data
-    socket_proxy = Networking::SocketProxy.build io
-
-    output = socket_proxy.read
-
-    assert output == data
-  end
-
-  test 'Read a specific amount of data' do
-    io = Networking::Controls::IO::Reading.example data
-    socket_proxy = Networking::SocketProxy.build io
-
-    output = socket_proxy.read 4
-
-    assert output == data[0...4]
-  end
-
-  test 'Read data into an output buffer' do
-    io = Networking::Controls::IO::Reading.example data
-    buffer = ''
-    socket_proxy = Networking::SocketProxy.build io
-
-    socket_proxy.read nil, buffer
-
-    assert buffer == data
-  end
-
-  test 'Data is read as ASCII-8BIT (binary)' do
-    io = Networking::Controls::IO::Reading.example data
-    socket_proxy = Networking::SocketProxy.build io
-
-    output = socket_proxy.read
-
-    assert output.encoding.name == 'ASCII-8BIT'
-  end
-
-  context 'Multiple fixed size reads are needed to fully read to EOF' do
-    test 'Data exceeds read buffer size' do
+    test 'Read until end of file is reached' do
       io = Networking::Controls::IO::Reading.example data
-      socket_proxy = Networking::SocketProxy.new io, io, 4
+      socket_proxy = Networking::SocketProxy.build io
 
       output = socket_proxy.read
 
       assert output == data
     end
 
-    test 'Interrupted by blocking reads' do
-      dispatcher = Networking::Controls::Scheduler::Cooperative::Dispatcher.new
-      scheduler = Networking::Scheduler::Cooperative.build dispatcher
+    test 'Read a specific amount of data' do
+      io = Networking::Controls::IO::Reading.example data
+      socket_proxy = Networking::SocketProxy.build io
 
-      output = nil
+      output = socket_proxy.read 4
 
-      Networking::Controls::IO::Scenarios::ReadsWillBlock.activate do |read_io, write_io|
-        socket_proxy = Networking::SocketProxy.build read_io, scheduler
+      assert output == data[0...4]
+    end
 
-        dispatcher.expect_read read_io do
-          write_io.write data[0...1]
-        end
-        dispatcher.expect_read read_io do
-          write_io.write data[1..-1]
-          write_io.close
-        end
+    test 'Read data into an output buffer' do
+      io = Networking::Controls::IO::Reading.example data
+      buffer = ''
+      socket_proxy = Networking::SocketProxy.build io
 
-        Networking::Controls::Scheduler::Cooperative::Fiber.run do
-          output = socket_proxy.read
-        end
+      socket_proxy.read nil, buffer
 
-        dispatcher.verify
+      assert buffer == data
+    end
+
+    test 'Data is read as ASCII-8BIT (binary)' do
+      io = Networking::Controls::IO::Reading.example data
+      socket_proxy = Networking::SocketProxy.build io
+
+      output = socket_proxy.read
+
+      assert output.encoding.name == 'ASCII-8BIT'
+    end
+
+    context 'Multiple fixed size reads are needed to fully read to EOF' do
+      test 'Data exceeds read buffer size' do
+        io = Networking::Controls::IO::Reading.example data
+        socket_proxy = Networking::SocketProxy.new io, io, 4
+
+        output = socket_proxy.read
+
+        assert output == data
       end
 
-      assert output == data
+      test 'Interrupted by blocking reads' do
+        dispatcher = Networking::Controls::Scheduler::Cooperative::Dispatcher.new
+        scheduler = Networking::Scheduler::Cooperative.build dispatcher
+
+        output = nil
+
+        Networking::Controls::IO::Scenarios::ReadsWillBlock.activate do |read_io, write_io|
+          socket_proxy = Networking::SocketProxy.build read_io, scheduler
+
+          dispatcher.expect_read read_io do
+            write_io.write data[0...1]
+          end
+          dispatcher.expect_read read_io do
+            write_io.write data[1..-1]
+            write_io.close
+          end
+
+          Networking::Controls::Scheduler::Cooperative::Fiber.run do
+            output = socket_proxy.read
+          end
+
+          dispatcher.verify
+        end
+
+        assert output == data
+      end
     end
   end
 end
--- test/spec/socket_proxy/write.rb
+++ test/spec/socket_proxy/write.rb
@@ -1,39 +1,41 @@
 require_relative './socket_proxy_spec_init'
 
-context 'Writing To a Socket Proxy' do
-  data = Networking::Controls::Data.example
+context 'Socket Proxy' do
+  context 'Writing' do
+    data = Networking::Controls::Data.example
 
-  test 'Single write transmits all the requested data' do
-    io = Networking::Controls::IO::Writing.example
-    socket_proxy = Networking::SocketProxy.build io
+    test 'Single write transmits all the requested data' do
+      io = Networking::Controls::IO::Writing.example
+      socket_proxy = Networking::SocketProxy.build io
 
-    bytes_written = socket_proxy.write data
+      bytes_written = socket_proxy.write data
 
-    assert io.string == data
-    assert bytes_written == data.bytesize
-  end
-
-  context 'Multiple writes are needed to fully write requested data' do
-    dispatcher = Networking::Controls::Scheduler::Cooperative::Dispatcher.new
-    scheduler = Networking::Scheduler::Cooperative.build dispatcher
-    write_buffer_window_size = Networking::Controls::IO::Scenarios::WritesWillBlock.write_buffer_window_size
-
-    output = nil
-
-    Networking::Controls::IO::Scenarios::WritesWillBlock.activate do |read_io, write_io|
-      socket_proxy = Networking::SocketProxy.build write_io, scheduler
-
-      dispatcher.expect_write write_io do
-        read_io.read write_buffer_window_size
-      end
-
-      Networking::Controls::Scheduler::Cooperative::Fiber.run do
-        output = socket_proxy.write data
-      end
-
-      dispatcher.verify
+      assert io.string == data
+      assert bytes_written == data.bytesize
     end
 
-    assert output == data.bytesize
+    context 'Multiple writes are needed to fully write requested data' do
+      dispatcher = Networking::Controls::Scheduler::Cooperative::Dispatcher.new
+      scheduler = Networking::Scheduler::Cooperative.build dispatcher
+      write_buffer_window_size = Networking::Controls::IO::Scenarios::WritesWillBlock.write_buffer_window_size
+
+      output = nil
+
+      Networking::Controls::IO::Scenarios::WritesWillBlock.activate do |read_io, write_io|
+        socket_proxy = Networking::SocketProxy.build write_io, scheduler
+
+        dispatcher.expect_write write_io do
+          read_io.read write_buffer_window_size
+        end
+
+        Networking::Controls::Scheduler::Cooperative::Fiber.run do
+          output = socket_proxy.write data
+        end
+
+        dispatcher.verify
+      end
+
+      assert output == data.bytesize
+    end
   end
 end
