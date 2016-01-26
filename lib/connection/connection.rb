0829812ba41f7ce222f0688675a1680ec0fb15fa
Nathan Ladd <nathan@obsidianexchange.com>
2016-01-26 10:30:09 -0600

    Inline use of logger in obsolete methods

--- lib/connection/connection.rb
+++ lib/connection/connection.rb
@@ -144,18 +144,16 @@ module Connection
   end
 
   def self.client(*arguments)
+    logger = Telemetry::Logger.get self
     logger.obsolete "Use of Connection.client is deprecated; prefer Connection::Client.build"
 
     Connection::Client.build *arguments
   end
 
   def self.server(*arguments)
+    logger = Telemetry::Logger.get self
     logger.obsolete "Use of Connection.server is deprecated; prefer Connection::Server.build"
 
     Connection::Server.build *arguments
-  end
-
-  def self.logger
-    @logger ||= Telemetry::Logger.get self
   end
 end
